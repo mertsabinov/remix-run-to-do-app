f8220a2c0cea62436d1cc61e3f9f9e1eb0ef3584
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-13 11:26:01 +0300

    update(Card): margin 0

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,16 +2,14 @@ import { Box, Text } from "@chakra-ui/react";
 
 export const Card = () => {
   return (
-    <Box marginLeft="5%" marginRight="5%">
-      <Box
-        backgroundColor="green.50"
-        borderRadius="10px"
-        marginTop="5%"
-        padding="3%"
-      >
-        <Text fontWeight="bold">Card</Text>
-        <Text>Card content</Text>
-      </Box>
+    <Box
+      backgroundColor="green.50"
+      borderRadius="10px"
+      marginTop="5%"
+      padding="3%"
+    >
+      <Text fontWeight="bold">Card</Text>
+      <Text>Card content</Text>
     </Box>
   );
 };
