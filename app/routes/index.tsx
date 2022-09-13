461607eaa28b69504ec1cfd5c5f908254aea625f
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-13 13:18:34 +0300

    add(index): fetch data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,35 @@
 import { Box, Text } from "@chakra-ui/react";
-import { Link } from "@remix-run/react";
+import { LoaderFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { Card } from "~/components/Card";
+import { Navbar } from "~/components/Navbar";
+
+export const loader: LoaderFunction = async () => {
+  const url = process.env.API_URL || "";
+  const res = await fetch(url).then((res) => res.json());
+  return { data: res };
+};
+
+type ToDo = {
+  id: string;
+  key: string;
+  value: string;
+};
+
+type LoaderData = {
+  data: ToDo[];
+};
 
 export const index = () => {
+  const { data } = useLoaderData<LoaderData>();
   return (
     <Box>
-      <Text>Hello</Text>
+      <Navbar />
+      {data.map(({ id, key, value }) => (
+        <Box id={id} marginTop="2%" marginLeft="25%" marginRight="25%">
+          <Card title={key} content={value} />
+        </Box>
+      ))}
     </Box>
   );
 };
